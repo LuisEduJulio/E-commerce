6f761f41c3acb938b0fe1acb9e759d8c362ccaf9
LuisEduJulio <luiseduardoer38@gmail.com>
2020-06-25 21:11:53 -0300

    Mudando repositório

---
+++ ApiCatalogo/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using APICatalogo.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCatalogo.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Nome { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string Descricao { get; set; }
+        [Required]
+        public double Preco { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string ImagemUrl { get; set; }
+        public int Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public Categoria Categorias { get; set; }
+        public int CategoriaId { get; set; }
+    }
+}
